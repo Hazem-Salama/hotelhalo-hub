417e13ee695c08151e43e813907aed2beb79f923
Hazem Ahmed Wagdy Hussein Salama <Thespecialoz108@gmail.com>
2026-01-07 10:51:46 +0000

    سس

--- server/HotelHaloHub.API/obj/Debug/net9.0/HotelHaloHub.API.AssemblyInfo.cs
+++ server/HotelHaloHub.API/obj/Debug/net9.0/HotelHaloHub.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HotelHaloHub.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d069d93e57c3ba58fa854283098e53ac46e5913")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96621ef5c0ce0868ebe188b1afcd1181df91820d")]
 [assembly: System.Reflection.AssemblyProductAttribute("HotelHaloHub.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HotelHaloHub.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
